c1aa99ea8ca84de19cbf8c0a079f5225106b6f8a
Murilo Henrique <openline_desenvolvimento5@outlook.com>
2019-07-05 13:33:50 -0300

    inicio

---
+++ Seção 05/App01ConsultarCEP/App01ConsultarCEP/App01ConsultarCEP/Servico/ViaCepServico.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App01ConsultarCEP.Servico
+{
+    public class ViaCepServico
+    {
+        private static string EnderecoURL = "https://viacep.com.br/ws/{0}/json/";
+
+        public static void BuscarEnderecoViaCep(string cep)
+        {
+            string NovoEnderecoURL = string.Format(EnderecoURL, cep);
+        }
+    }
+}
